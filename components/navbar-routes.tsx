7878c95f1851fdb1c8eaf1fa7dcf03df9b6acece
victhangnguyen <97330371+victhangnguyen@users.noreply.github.com>
2024-01-08 00:39:27 +0700

    update courses and analytics page of teacher route, update teacher route for navbar-routes, and update Menu button for mobile-sidebar.

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,20 +1,33 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
-import { usePathname, useRouter } from "next/navigation";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
 
 const NavbarRoutes = () => {
   const pathname = usePathname();
-  const router = useRouter();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname.includes("/chapter");
 
-  
-
   return (
     <div className="ml-auto flex gap-x-2">
-      <UserButton />
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/">
+          <Button>
+            <LogOut className="mr-2 h-4 w-4" /> Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
